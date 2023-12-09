69b36762ae48613c110c01294af4b6f9d4eaa9c2
fady mohamed abo alhassan <132343355+Fady4Mohamed@users.noreply.github.com>
2023-12-09 11:41:12 -1000

    add auth cubit

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  
+WidgetsFlutterBinding.ensureInitialized();
  await Firebase.initializeApp(
   options: DefaultFirebaseOptions.currentPlatform,
-);  Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+); 
   runApp(const MyApp());
 }
 
