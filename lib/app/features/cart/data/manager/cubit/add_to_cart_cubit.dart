eb158422a14879e3f14172479d084c6c781e033f
fady mohamed abo alhassan <132343355+Fady4Mohamed@users.noreply.github.com>
2024-03-11 04:28:56 -0900

    add orders to datadase

--- lib/app/features/cart/data/manager/cubit/add_to_cart_cubit.dart
+++ lib/app/features/cart/data/manager/cubit/add_to_cart_cubit.dart
@@ -4,7 +4,7 @@ import 'package:caleb_g/app/core/manager/models/FoodModel.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
-
+import 'dart:core';
 part 'add_to_cart_state.dart';
 
 class AddToCartCubit extends Cubit<AddToCartState> {
@@ -32,10 +32,24 @@ class AddToCartCubit extends Cubit<AddToCartState> {
   for (var i = 0; i < cartinapp.length; i++) {
    await comp(foodid: cartinapp[i].id, amount: amountlist[i]);
     }
+    DocumentReference order = FirebaseFirestore.instance.collection('orders').doc();
+    order.set({
+      'cart': '${username}cart',
+      'adresse': '2 said ashta street',
+      'date':  DateTime.now().toString(),
+      'orderid':order.id,
+    }).then((value) {
+    
+    })  // ignore: invalid_return_type_for_catch_error
+        .catchError((error) {
+      emit(AddToCartfailure(error: error.toString()));
+    });
    emit(AddToCartsuccess());
    await Future.delayed(Duration(seconds: 1));
    cartinapp.clear();
    amountlist.clear();
+   salary=0;
+   refresh();
 }
   }
   refresh(){
